cbd845ccc573ec5c71ca5a209c04b984c3f25404
Kishan-hyalij <kishanhyalij@gmail.com>
2025-10-09 01:10:05 +0530

    exception handeling implimented

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,19 +4,35 @@ import pandas as pd
 from logger import logging
 import os
 
+logging.info("data ingestion start")
+
 # Data Collection
 logging.info("reading data")
-Data=pd.read_csv('data/raw/Telco-Customer-Churn.csv')
-logging.info("data read successfully")
+try:
+    Data=pd.read_csv('data/raw/Telco-Customer-Churn.csv')
+    logging.info("data read successfully")
+except Exception as e:
+            logging.info("data read failed")
+            raise(e)
 
 # Setting path to save data csv 
 logging.info("path setting-up")
-processed_data=os.path.join('data','processed')
-os.makedirs(processed_data,exist_ok=True)
-processed_data_path=os.path.join('data','processed',"data.csv")
-logging.info("path set")
+try:
+    processed_data=os.path.join('data','processed')
+    os.makedirs(processed_data,exist_ok=True)
+    processed_data_path=os.path.join('data','processed',"data.csv")
+    logging.info("path set")
+except Exception as e:
+       logging.info("path failed")
+       raise(e)
 
 # Saving data csv
-logging.info("saving csv")
-Data.to_csv(processed_data_path)
-logging.info("csv saved")
+try:
+    logging.info("saving csv")
+    Data.to_csv(processed_data_path)
+    logging.info("csv saved")
+except Exception as e:
+        logging.info("csv not saved")
+        raise(e)
+
+logging.info("data ingestion end")
